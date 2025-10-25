86e45e783882ab947acaa328b2675e6606cd1788
Rogkk <rogerioweel@gmail.com>
2025-10-25 10:45:15 -0300

    estudo de flask, rotas e POST

---
+++ auth/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        name = request.form.get('name', 'name')
+        if name == 'Admin':
+            return f'Logado {name}'
+        else:
+            return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
